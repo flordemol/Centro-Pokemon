d3ee35864ee2b662ed1df2c6df934d2f91a5e019
Florencia De Mollein <florenciademollein@gmail.com>
2022-03-14 13:17:36 -0300

    Instalación de React Query y agregué el Provider por encima de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from  "react-query";
 import Home from "./components/Home/Home";
 import Formulario from "./components/Formulario/Formulario";
 import "./App.css";
 
 function App() {
+
+  const queryClient = new QueryClient();
+
+  // Agregamos Provider de react-query para poder usar en toda la app esta instancia de QueryClient con el Hook useQuery
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/formularioIngreso" element={<Formulario />} />
-      </Routes>
-    </div>
+    <QueryClientProvider client={ queryClient }>
+      <div className="App">
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/formularioIngreso" element={<Formulario />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
